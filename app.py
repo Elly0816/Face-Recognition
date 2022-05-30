c1950d785034bcabff82b7b2943f53a2df391dbe
Eleazar Udo <elzoremmanuel@gmail.com>
2022-05-30 15:58:42 +0300

    building api

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+
+load_dotenv()
+port = os.getenv('PORT')
+
+app = Flask(__name__)
+
+#TODO 1: Create route to load image into face folder,
+# blur images where the face appears from the images folder,
+# and then returns the blurred images as a file.
+# The image sent should be removed from the folder after the operation
+
+
+#TODO 2: Create route to load images into the images folder
+# This returns a message of the status of the save
+
+
+
+if __name__ == '__main__':
+    app.run(port=port, debug=True)
