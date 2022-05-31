854ac265074ba0bf3244c26fa3a0cd41ba00d95a
Eleazar Udo <elzoremmanuel@gmail.com>
2022-06-01 01:21:18 +0300

    added CORS to api

--- api/app.py
+++ api/app.py
@@ -1,31 +1,45 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 from recognize_face import RecognizeFace
 from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
 
 """This is the endpoint that the react app would make requests to"""
 load_dotenv()
 port = os.getenv('PORT')
-ALLOWED_EXTENSIONS = ['']
+ALLOWED_EXTENSIONS = ['png', 'jpeg', 'jpg']
+
+def allowed_file(filename):
+    if '.' in filename and filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS:
+        return True
+    else:
+        return False
 
 app = Flask(__name__)
+CORS(app)
 
 #TODO 1: Create route to load image into face folder,
 # blur images where the face appears from the images folder,
 # and then returns the blurred images as a file.
 # The image sent should be removed from the folder after the operation
-@app.route("/find")
-def find():
-    pass
+@app.route("/find", methods=['POST'])
+def upload_file():
+   pass
+            
+        
     
 
 
 #TODO 2: Create route to save images into the images folder
 # This returns a message of the status of the save
-@app.route("/save")
-def save():
-    pass
+@app.route("/save", methods=['POST'])
+def upload_filen():
+     if request.method == 'POST':
+            #Check if the post request has the file part
+        if 'file' not in request.files:
+            pass
+            
 
 
 
